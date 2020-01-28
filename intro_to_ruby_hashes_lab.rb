c4dabbd052535c97234fa33d6dbad8cb7bd67300
Christopher Leja <christopher.leja@gmail.com>
2020-01-28 17:28:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,19 @@
-def base_hash(key, value)
-  base_hash = {key: value}
+def base_hash
+  base_hash = {monopoly_with_second_tier: {}}
   base_hash
 end
 
 def monopoly_with_second_tier
-  base_hash = {}
+  monopoly_with_second_tier = {monopoly_with_third_tier: {}}
   base_hash
 end
 
 def monopoly_with_third_tier
-  base_hash = {}
+  monopoly_with_third_tier = {monopoly_with_fourth_tier: {}}
   base_hash
 end
 
 def monopoly_with_fourth_tier
-  base_hash = {}
+  monopoly_with_fourth_tier = {monopoly_with_fifth_tier: {}}
   base_hash
 end
