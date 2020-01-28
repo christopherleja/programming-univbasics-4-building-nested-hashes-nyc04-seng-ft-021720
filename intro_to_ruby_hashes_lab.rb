7b9912ecab0c101516641de8558416601b5763cb
Christopher Leja <christopher.leja@gmail.com>
2020-01-28 18:02:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,9 @@ def monopoly_with_third_tier
     pieces: 4,
     rent_in_dollars: {
       one_piece_owned: 25,
-      2 => {},
-      3 => {},
-      4 => {},
+      two_pieces_owned: 50,
+      three_pieces_owned: 75,
+      four_pieces_owned: 100
     },
     names: {
       1 => {},
