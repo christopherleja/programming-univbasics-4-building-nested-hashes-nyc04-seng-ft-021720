0b807f32bdaae2ad0f9c6cfcd9c362109a12a6c0
Christopher Leja <christopher.leja@gmail.com>
2020-01-28 17:53:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,16 @@
 def base_hash
   base_hash = {railroads: {}}
-  base_hash
 end
 
 def monopoly_with_second_tier
   base_hash = {railroads: {
-    pieces: {}
+    pieces: 4
   }
   }
 end
 
 def monopoly_with_third_tier
-  monopoly_with_third_tier = {monopoly_with_fourth_tier: {}}
+  base_hash = {monopoly_with_fourth_tier: {}}
   base_hash
 end
 
